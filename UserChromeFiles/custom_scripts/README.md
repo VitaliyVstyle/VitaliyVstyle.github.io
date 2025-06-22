3f6abfdcdc1f8ad33af32b5b10dab1a9725ce96e
VitaliyVstyle <serials101@gmail.com>
2025-06-22 13:07:55 +0300

    2025.06.22

--- UserChromeFiles/custom_scripts/README.md
+++ UserChromeFiles/custom_scripts/README.md
@@ -4,3 +4,5 @@
 Run applications  
 [loadsFavicons](https://github.com/VitaliyVstyle/VitaliyVstyle.github.io/blob/main/UserChromeFiles/custom_scripts/loadsFavicons.js)  
 Restore favicons  
+[openInLibrary](https://github.com/VitaliyVstyle/VitaliyVstyle.github.io/blob/main/UserChromeFiles/custom_scripts/openInLibrary.js)  
+Open in library  
---
+++ UserChromeFiles/custom_scripts/openInLibrary.js
@@ -0,0 +1,90 @@
+/**
+@UCF @param {"prop":"JsAllChrome.load","ucfobj":true,"urlregxp":"^chrome:\\/\\/browser\\/content\\/(?:browser|places\\/bookmarksSidebar)\\.xhtml"} @UCF
+*/
+(async (
+    label = "Open in library",
+    image = "chrome://browser/skin/library.svg",
+    openFoldersInRightPane = false,
+) => ({
+    init() {
+        var popup = this.popup = document.querySelector("#placesContext");
+        if (!popup) return;
+        setUnloadMap(Symbol(), this.destructor, this);
+        var item = this.item = document.createXULElement("menuitem");
+        item.id = "placesContext_open:library";
+        item.setAttribute("label", label);
+        if (image) {
+            item.className = "menuitem-iconic";
+            item.style.cssText = `list-style-image:url("${image}");-moz-context-properties:fill;fill:currentColor;`;
+        }
+        item.setAttribute("selection-type", "single");
+        item.setAttribute("node-type", "link_bookmark|folder");
+        item.addEventListener("command", this);
+        (popup.querySelector("#placesContext_openSeparator, menuseparator") || popup.querySelector("menuseparator")).before(item);
+    },
+    handleEvent() {
+        var tn = this.popup.triggerNode;
+        if (!tn) return;
+        var node;
+        if (tn.matches("treechildren")) node = tn.parentElement.selectedNode;
+        else node = tn._placesNode || this.popup._view.result.root;
+        if (!node) return;
+        var data = [
+            node.bookmarkGuid,
+            node.parent?.bookmarkGuid,
+            (PlacesUtils.nodeIsFolderOrShortcut || PlacesUtils.nodeIsFolder)(node),
+            node.pageGuid
+        ];
+        var win = Services.wm.getMostRecentWindow("Places:Organizer");
+        if (win) return win.focus(this.onLibrary(win, data));
+        win = openDialog("chrome://browser/content/places/places.xhtml", "", "chrome,toolbar=yes,dialog=no,resizable");
+        win.addEventListener("pageshow", () => this.onLibrary(win, data), { once: true });
+    },
+    async onLibrary(win, data) {
+        var [guid, parentGuid, isFolder, pageGuid] = data;
+        var list = win.document.querySelector("#placesList");
+        var tree = win.document.querySelector("#placeContent");
+        var onlyLeft = isFolder && !openFoldersInRightPane;
+        var search = tree.result.root.uri.startsWith("place:terms=");
+        var vRoot = PlacesUtils.virtualAllBookmarksGuid;
+        if (!onlyLeft || search) {
+            if (PlacesUtils.bookmarks.userContentRoots.includes(guid)) parentGuid = vRoot;
+            else if (!parentGuid) parentGuid = (await PlacesUtils.bookmarks.fetch(guid))?.parentGuid;
+        }
+        if (search) {
+            var trgGuid = onlyLeft ? guid : parentGuid;
+            if (PlacesUtils.getConcreteItemGuid(list.selectedNode) == trgGuid) list.selectItems([vRoot]);
+            else {
+                var rows = list.view._rows, lastRow = rows[rows.length - 1];
+                if (lastRow.bookmarkGuid == vRoot) lastRow.containerOpen = true;
+            }
+        }
+        if (onlyLeft) {
+            list.selectItems([guid]);
+            return list.focus(this.scroll(list));
+        }
+        list.selectItems([parentGuid]);
+        this.scroll(list);
+        if (guid) tree.selectItems([guid]);
+        else if (pageGuid) {
+            var ind = tree.view._rows.findIndex(r => r.pageGuid && r.pageGuid == pageGuid);
+            if (ind != -1) {
+                tree.view.selection.clearSelection();
+                tree.view.selection.rangedSelect(ind, ind, true);
+            }
+        }
+        await new Promise(resolve => setTimeout(resolve, 100));
+        tree.focus(this.scroll(tree));
+    },
+    scroll(tree) {
+        var pos = .35;
+        var visibleRows = tree.getPageLength();
+        var ind = tree.view.selection.currentIndex;
+        var first = tree.getFirstVisibleRow();
+        var newFirst = ind - pos * visibleRows + 1;
+        tree.scrollByLines(Math.round(newFirst - first));
+    },
+    destructor() {
+        this.item.removeEventListener("command", this);
+    },
+}).init())();

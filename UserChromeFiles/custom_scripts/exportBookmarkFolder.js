e70d7433c1cecaed7e0bd88f5a01047ec61ff5c7
VitaliyVstyle <serials101@gmail.com>
2025-06-30 20:25:08 +0300

    2025.06.30

--- UserChromeFiles/custom_scripts/exportBookmarkFolder.js
+++ UserChromeFiles/custom_scripts/exportBookmarkFolder.js
@@ -3,50 +3,50 @@
 @UCF @param {"prop":"JsContent.pageshow","urlregxp":"^chrome:\\/\\/browser\\/content\\/(?:browser|places\\/(?:bookmarksSidebar|places))\\.xhtml"} @UCF
 */
 (async (
-	label = "Export folder to HTML",
-	image = "chrome://global/skin/icons/arrows-updown.svg",
+    label = "Export folder to HTML",
+    image = "chrome://global/skin/icons/arrows-updown.svg",
 ) => ({
-	get dps() {
-		delete this.dps;
-		return this.dps = windowRoot.ownerGlobal.DownloadPaths || ChromeUtils.importESModule("resource://gre/modules/DownloadPaths.sys.mjs").DownloadPaths;
-	},
-	get exporter() {
-		delete this.exporter;
-		return this.exporter = windowRoot.ownerGlobal.UcfPrefs.dbg.ref("BookmarkExporter",
-			(windowRoot.ownerGlobal.BookmarkHTMLUtils || ChromeUtils.importESModule("resource://gre/modules/BookmarkHTMLUtils.sys.mjs").BookmarkHTMLUtils).exportToFile);
-	},
-	init() {
-		var sep = document.querySelector("#placesContext > #placesContext_openSeparator");
-		if (!sep) return;
-		var item = document.createXULElement("menuitem");
-		item.id = "placesContext_exportFolder";
-		item.label = label;
-		if (image) {
-			item.className = "menuitem-iconic";
-			item.style.cssText = `list-style-image:url("${image}");-moz-context-properties:fill;fill:currentColor;`;
-		}
-		item.setAttribute("selection-type", "single");
-		item.setAttribute("node-type", "folder");
-		item.onclick = this.export.bind(this, sep.parentElement);
-		sep.before(item);
-	},
-	async export({triggerNode: tn, _view: vw}) {
-		var pu = PlacesUtils, bm = pu.bookmarks, node;
-		if (tn.matches("treechildren")) node = vw.selectedNode;
-		else node = tn._placesNode;
-		if (!node) node = vw.result.root;
-		var fp = Cc["@mozilla.org/filepicker;1"].createInstance(Ci.nsIFilePicker);
-		fp.init(browsingContext, PlacesUIUtils.promptLocalization.formatValueSync("places-bookmarks-export"), fp.modeSave);
-		fp.appendFilters(fp.filterHTML);
-		fp.defaultString = `${(node.title ? this.dps.sanitize(node.title) : "untitled")}.html`;
-		if (await new Promise(fp.open) !== fp.returnOK) return;
-		var tree = await pu.promiseBookmarksTree(pu.getConcreteItemGuid(node), {includeItemIds: true});
-		tree.title = bm.getLocalizedTitle(tree);
-		var bookmarks = {children: [
-			{root: "toolbarFolder"},
-			{root: "unfiledBookmarksFolder"},
-			{root: "bookmarksMenuFolder", children: [tree], guid: bm.menuGuid}
-		]};
-		new this.exporter(bookmarks).exportToFile(fp.file.path);
-	},
+    get dps() {
+        delete this.dps;
+        return this.dps = windowRoot.ownerGlobal.DownloadPaths || ChromeUtils.importESModule("resource://gre/modules/DownloadPaths.sys.mjs").DownloadPaths;
+    },
+    get exporter() {
+        delete this.exporter;
+        return this.exporter = windowRoot.ownerGlobal.UcfPrefs.dbg.ref("BookmarkExporter",
+            (windowRoot.ownerGlobal.BookmarkHTMLUtils || ChromeUtils.importESModule("resource://gre/modules/BookmarkHTMLUtils.sys.mjs").BookmarkHTMLUtils).exportToFile);
+    },
+    init() {
+        var sep = document.querySelector("#placesContext > #placesContext_openSeparator");
+        if (!sep) return;
+        var item = document.createXULElement("menuitem");
+        item.id = "placesContext_exportFolder";
+        item.label = label;
+        if (image) {
+            item.className = "menuitem-iconic";
+            item.style.cssText = `list-style-image:url("${image}");-moz-context-properties:fill;fill:currentColor;`;
+        }
+        item.setAttribute("selection-type", "single");
+        item.setAttribute("node-type", "folder");
+        item.onclick = this.export.bind(this, sep.parentElement);
+        sep.before(item);
+    },
+    async export({triggerNode: tn, _view: vw}) {
+        var pu = PlacesUtils, bm = pu.bookmarks, node;
+        if (tn.matches("treechildren")) node = vw.selectedNode;
+        else node = tn._placesNode;
+        if (!node) node = vw.result.root;
+        var fp = Cc["@mozilla.org/filepicker;1"].createInstance(Ci.nsIFilePicker);
+        fp.init(browsingContext, PlacesUIUtils.promptLocalization.formatValueSync("places-bookmarks-export"), fp.modeSave);
+        fp.appendFilters(fp.filterHTML);
+        fp.defaultString = `${(node.title ? this.dps.sanitize(node.title) : "untitled")}.html`;
+        if (await new Promise(fp.open) !== fp.returnOK) return;
+        var tree = await pu.promiseBookmarksTree(pu.getConcreteItemGuid(node), {includeItemIds: true});
+        tree.title = bm.getLocalizedTitle(tree);
+        var bookmarks = {children: [
+            {root: "toolbarFolder"},
+            {root: "unfiledBookmarksFolder"},
+            {root: "bookmarksMenuFolder", children: [tree], guid: bm.menuGuid}
+        ]};
+        new this.exporter(bookmarks).exportToFile(fp.file.path);
+    },
 }).init())();
--- UserChromeFiles/custom_scripts/toggleFoldersScrollPositionTree.js
+++ UserChromeFiles/custom_scripts/toggleFoldersScrollPositionTree.js
@@ -4,145 +4,145 @@
 @UCF @param {"prop":"JsContent.pageshow","urlregxp":"^chrome:\\/\\/browser\\/content\\/places\\/(?:bookmarksSidebar|historySidebar|places)\\.xhtml"} @UCF
 */
 (async (
-	id = "ucf-toggle-folders-scroll-position-tree",
-	tooltipText = "Left-click: Close all top level folders\n((Ctrl | Shift) Left-click) | Midle-click: Close all folders\nRight-click: Open all folders",
+    id = "ucf-toggle-folders-scroll-position-tree",
+    tooltipText = "Left-click: Close all top level folders\n((Ctrl | Shift) Left-click) | Midle-click: Close all folders\nRight-click: Open all folders",
     image = "data:image/svg+xml;charset=utf-8,<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 16 16'><path style='fill:none;stroke:context-fill rgb(142, 142, 152);stroke-opacity:context-fill-opacity;stroke-width:1.2;stroke-linecap:round;stroke-linejoin:round;' d='m 1.5,3.5 0,10 c 0,0 0,1 1,1 h 11 c 0,0 1,0 1,-1 v -8 c 0,-1 -1,-1 -1,-1 h -5 l -2,-2 h -4 c 0,0 -1,0 -1,1 z'/></svg>",
-	scrollPosition = true,
-	scrollPositionPref = "ucf.toggle_folders.position",
+    scrollPosition = true,
+    scrollPositionPref = "ucf.toggle_folders.position",
 ) => ({
-	timer: null,
-	init() {
-		if (globalThis.UcfPrefs?.customSandbox == globalThis) return Services.io.getProtocolHandler("resource")
-			.QueryInterface(Ci.nsIResProtocolHandler)
-			.setSubstitution(id, Services.io.newURI(image));
-		var toolbar = document.querySelector("hbox#sidebar-search-container, toolbar#placesToolbar");
-		var tree = this.tree = document.querySelector("tree.sidebar-placesTree, tree.placesTree");
-		if (!toolbar || !tree) return;
-		var btn = document.createXULElement("toolbarbutton");
-		btn.id = id;
-		btn.onclick = this.toggle.bind(this);
-		btn.tooltipText = tooltipText;
-		Object.defineProperty(btn, "hidden", {});
-		Object.defineProperty(btn, "disabled", {});
-		toolbar.prepend(btn);
-		var sheet = new document.ownerGlobal.CSSStyleSheet();
-		sheet.replaceSync(`
-			#${id} {
-				margin-inline: 0 2px !important;
-				margin-block: 0 !important;
-				padding: 4px !important;
-				border: none !important;
-				border-radius: var(--border-radius-small, 0) !important;
-				min-width: 0 !important;
-				appearance: none !important;
-				background: none !important;
-				align-self: center !important;
-				list-style-image: url("resource://${id}") !important;
-				-moz-context-properties: fill, stroke, fill-opacity;
-				fill: currentColor;
-				&:hover {
-					background: light-dark(rgba(0, 0, 0, .1), rgba(255, 255, 255, .15)) !important;
-				}
-				&:hover:active {
-					background: light-dark(rgba(0, 0, 0, .15), rgba(255, 255, 255, .1)) !important;
-				}
-			}
-		`);
-		document.adoptedStyleSheets.push(sheet);
-		if (!scrollPosition || !(this.searchbox = document.querySelector("#search-box, #searchFilter"))) return scrollPosition = false;
-		try {
-			setUnloadMap(Symbol(id), this.destructor, this);
-		} catch {
-			window.addEventListener("unload", () => this.destructor(), {once: true});
-		}
-		this.treeId = tree.id;
-		var treeBody = this.treeBody = tree.treeBody;
-		treeBody.addEventListener("scroll", this);
-		treeBody.addEventListener("underflow", this);
-		treeBody.addEventListener("overflow", this);
-		this.searchbox.addEventListener("MozInputSearch:search", this);
-		this.scrollPosition();
+    timer: null,
+    init() {
+        if (globalThis.UcfPrefs?.customSandbox == globalThis) return Services.io.getProtocolHandler("resource")
+            .QueryInterface(Ci.nsIResProtocolHandler)
+            .setSubstitution(id, Services.io.newURI(image));
+        var toolbar = document.querySelector("hbox#sidebar-search-container, toolbar#placesToolbar");
+        var tree = this.tree = document.querySelector("tree.sidebar-placesTree, tree.placesTree");
+        if (!toolbar || !tree) return;
+        var btn = document.createXULElement("toolbarbutton");
+        btn.id = id;
+        btn.onclick = this.toggle.bind(this);
+        btn.tooltipText = tooltipText;
+        Object.defineProperty(btn, "hidden", {});
+        Object.defineProperty(btn, "disabled", {});
+        toolbar.prepend(btn);
+        var sheet = new document.ownerGlobal.CSSStyleSheet();
+        sheet.replaceSync(`
+            #${id} {
+                margin-inline: 0 2px !important;
+                margin-block: 0 !important;
+                padding: 4px !important;
+                border: none !important;
+                border-radius: var(--border-radius-small, 0) !important;
+                min-width: 0 !important;
+                appearance: none !important;
+                background: none !important;
+                align-self: center !important;
+                list-style-image: url("resource://${id}") !important;
+                -moz-context-properties: fill, stroke, fill-opacity;
+                fill: currentColor;
+                &:hover {
+                    background: light-dark(rgba(0, 0, 0, .1), rgba(255, 255, 255, .15)) !important;
+                }
+                &:hover:active {
+                    background: light-dark(rgba(0, 0, 0, .15), rgba(255, 255, 255, .1)) !important;
+                }
+            }
+        `);
+        document.adoptedStyleSheets.push(sheet);
+        if (!scrollPosition || !(this.searchbox = document.querySelector("#search-box, #searchFilter"))) return scrollPosition = false;
+        try {
+            setUnloadMap(Symbol(id), this.destructor, this);
+        } catch {
+            window.addEventListener("unload", () => this.destructor(), {once: true});
+        }
+        this.treeId = tree.id;
+        var treeBody = this.treeBody = tree.treeBody;
+        treeBody.addEventListener("scroll", this);
+        treeBody.addEventListener("underflow", this);
+        treeBody.addEventListener("overflow", this);
+        this.searchbox.addEventListener("MozInputSearch:search", this);
+        this.scrollPosition();
     },
     handleEvent(e) {
         this[e.type](e);
     },
-	underflow() {
-		this._underflow = true;
-	},
-	overflow() {
-		this._underflow = false;
-	},
-	scroll() {
-		clearTimeout(this.timer);
-		if (this._underflow) return;
-		this.timer = setTimeout(() => {
-			var {searchbox, tree, treeId} = this;
-			if (!searchbox.value) Services.prefs.setIntPref(`${scrollPositionPref}_${treeId}`, tree.getFirstVisibleRow());
-		}, 500);
-	},
-	select(e) {
-		e.stopImmediatePropagation();
-	},
-	"MozInputSearch:search"() {
-		this.scrollPosition();
-	},
-	scrollPosition() {
-		var {searchbox, tree, treeId} = this;
-		if (!searchbox.value) tree.scrollToRow(Services.prefs.getIntPref(`${scrollPositionPref}_${treeId}`, 0));
-	},
-	toggle(e) {
-		if (this.start) return;
-		var {view} = this.tree;
-		if (view._isPlainContainer(view._rootNode)) return;
-		this.start = true;
-		var close = e.button < 2;
-		var closeAll = e.button === 1 || e.button === 0 && (e.ctrlKey || e.shiftKey);
-		var index = view.rowCount, open, roots, sel;
-		if (close) {
-			sel = view.selection;
-			let {count} = sel;
-			if (count) {
-				roots = new Set();
-				let currRoot;
-				for (let ind = 0; ind < index; ind++) {
-					let node = view._rows[ind];
-					if (node.indentLevel === 0) currRoot = node;
-					if (sel.isSelected(ind)) {
-						roots.add(currRoot);
-						if (!--count) break;
-					}
-				}
-			}
-			this.tree.addEventListener("select", this, true);
-		} else open = true;
-		if (closeAll) for (let ind = index; ind >= 0; ind--) view.isContainer(ind) && view.isContainerOpen(ind) && view.toggleOpenState(ind);
-		else {
-			let TFS = Ci.nsINavHistoryResultNode.RESULT_TYPE_FOLDER_SHORTCUT;
-			for (let ind = 0; ind < index; ind++) {
-				if (!view.isContainer(ind) || view.isContainerOpen(ind) !== close) continue;
-				if (open) {
-					let node = view._rows[ind];
-					if (node.type === TFS && node.indentLevel && !PlacesUtils.bookmarks.isVirtualRootItem(node.bookmarkGuid)) continue;
-				}
-				view.toggleOpenState(ind);
-				index = view.rowCount;
-		    }
-	    }
-		if (roots) {
-			sel.clearSelection();
-			for (let ind = 0, len = view.rowCount; ind < len; ind++)
-				roots.has(view._rows[ind]) && sel.rangedSelect(ind, ind, true);
-			roots.clear();
-		}
-		if (close) this.tree.removeEventListener("select", this, true);
-		else if (scrollPosition) this.scrollPosition();
-		this.start = false;
-	},
-	destructor() {
-		var {treeBody} = this;
-		treeBody.removeEventListener("scroll", this);
-		treeBody.removeEventListener("underflow", this);
-		treeBody.removeEventListener("overflow", this);
-		this.searchbox.removeEventListener("MozInputSearch:search", this);
+    underflow() {
+        this._underflow = true;
+    },
+    overflow() {
+        this._underflow = false;
+    },
+    scroll() {
+        clearTimeout(this.timer);
+        if (this._underflow) return;
+        this.timer = setTimeout(() => {
+            var {searchbox, tree, treeId} = this;
+            if (!searchbox.value) Services.prefs.setIntPref(`${scrollPositionPref}_${treeId}`, tree.getFirstVisibleRow());
+        }, 500);
+    },
+    select(e) {
+        e.stopImmediatePropagation();
+    },
+    "MozInputSearch:search"() {
+        this.scrollPosition();
+    },
+    scrollPosition() {
+        var {searchbox, tree, treeId} = this;
+        if (!searchbox.value) tree.scrollToRow(Services.prefs.getIntPref(`${scrollPositionPref}_${treeId}`, 0));
+    },
+    toggle(e) {
+        if (this.start) return;
+        var {view} = this.tree;
+        if (view._isPlainContainer(view._rootNode)) return;
+        this.start = true;
+        var close = e.button < 2;
+        var closeAll = e.button === 1 || e.button === 0 && (e.ctrlKey || e.shiftKey);
+        var index = view.rowCount, open, roots, sel;
+        if (close) {
+            sel = view.selection;
+            let {count} = sel;
+            if (count) {
+                roots = new Set();
+                let currRoot;
+                for (let ind = 0; ind < index; ind++) {
+                    let node = view._rows[ind];
+                    if (node.indentLevel === 0) currRoot = node;
+                    if (sel.isSelected(ind)) {
+                        roots.add(currRoot);
+                        if (!--count) break;
+                    }
+                }
+            }
+            this.tree.addEventListener("select", this, true);
+        } else open = true;
+        if (closeAll) for (let ind = index; ind >= 0; ind--) view.isContainer(ind) && view.isContainerOpen(ind) && view.toggleOpenState(ind);
+        else {
+            let TFS = Ci.nsINavHistoryResultNode.RESULT_TYPE_FOLDER_SHORTCUT;
+            for (let ind = 0; ind < index; ind++) {
+                if (!view.isContainer(ind) || view.isContainerOpen(ind) !== close) continue;
+                if (open) {
+                    let node = view._rows[ind];
+                    if (node.type === TFS && node.indentLevel && !PlacesUtils.bookmarks.isVirtualRootItem(node.bookmarkGuid)) continue;
+                }
+                view.toggleOpenState(ind);
+                index = view.rowCount;
+            }
+        }
+        if (roots) {
+            sel.clearSelection();
+            for (let ind = 0, len = view.rowCount; ind < len; ind++)
+                roots.has(view._rows[ind]) && sel.rangedSelect(ind, ind, true);
+            roots.clear();
+        }
+        if (close) this.tree.removeEventListener("select", this, true);
+        else if (scrollPosition) this.scrollPosition();
+        this.start = false;
+    },
+    destructor() {
+        var {treeBody} = this;
+        treeBody.removeEventListener("scroll", this);
+        treeBody.removeEventListener("underflow", this);
+        treeBody.removeEventListener("overflow", this);
+        this.searchbox.removeEventListener("MozInputSearch:search", this);
     },
 }).init())();

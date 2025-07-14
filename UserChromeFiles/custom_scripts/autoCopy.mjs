863eb587ada44668d6147900cc4a6827a13ccfb2
VitaliyVstyle <serials101@gmail.com>
2025-07-14 20:18:20 +0300

    2025.07.14

---
+++ UserChromeFiles/custom_scripts/autoCopy.mjs
@@ -0,0 +1,38 @@
+/**
+@UCF @param {"prop":"JsBackground","module":{"name":"autoCopy","child":{"events":{"pageshow":{},"pagehide":{}}},"allFrames":true,"messageManagerGroups":["browsers"],"matches":["<all_urls>","about:srcdoc"]}} @UCF
+*/
+const lazy = {
+    noBlink: false, // Selected text blinks when autocopying
+
+    get reasons() {
+        delete this.reasons;
+        return this.reasons = new Set(["MOUSEUP", "KEYPRESS", "SELECTALL"].map(reason => Ci.nsISelectionListener[`${reason}_REASON`]));
+    },
+};
+export class autoCopyChild extends JSWindowActorChild {
+    handleEvent(e) {
+        this[e.type](e);
+    }
+    pageshow() {
+        var notifySelectionChanged = this.changed.bind(this);
+        (this.sel = this.document.getSelection())?.addSelectionListener(this.listener = { notifySelectionChanged });
+    }
+    pagehide() {
+        this.sel?.removeSelectionListener(this.listener);
+    }
+    changed(doc, sel, reason) {
+        if (!lazy.reasons.has(reason) || !/\S/.test(sel)) return;
+        var ds = this.docShell;
+        ds.doCommand("cmd_copy");
+        if (lazy.noBlink) return;
+        var sc = ds.QueryInterface(Ci.nsIInterfaceRequestor)
+            .getInterface(Ci.nsISelectionDisplay)
+            .QueryInterface(Ci.nsISelectionController);
+        this.contentWindow.setTimeout(this.repaint, 50, sc, sc.SELECTION_OFF);
+        this.contentWindow.setTimeout(this.repaint, 200, sc, sc.SELECTION_ON);
+    }
+    repaint(sc, disp) {
+        sc.setDisplaySelection(disp);
+        sc.repaintSelection(sc.SELECTION_NORMAL);
+    }
+}

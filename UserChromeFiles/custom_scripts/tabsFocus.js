75618dadfd0dc577657d88ad9588942950e50a75
VitaliyVstyle <serials101@gmail.com>
2026-01-14 00:34:52 +0300

    2026.01.14

--- UserChromeFiles/custom_scripts/tabsFocus.js
+++ UserChromeFiles/custom_scripts/tabsFocus.js
@@ -9,81 +9,80 @@
 ) => ({
     init() {
         this.tid = Cc["@mozilla.org/timer;1"].createInstance(Ci.nsITimer);
-        this.target = false;
+        this.type = this.tid.TYPE_ONE_SHOT;
         var tabs = this.tabs = gBrowser.tabContainer;
         if (clickreloadtab) this.reload = document.querySelector("commandset#mainCommandSet > command[id='Browser:Reload']");
         setUnloadMap(Symbol(), this.destructor, this);
         if (previewmode) {
+            this.pwTid = Cc["@mozilla.org/timer;1"].createInstance(Ci.nsITimer);
             this.onMouseIn = this._onMouseIn;
             this.onMouseOut = this._onMouseOut;
-            this.onMouseClicked = this._onMouseClicked;
-            this.previewTid = Cc["@mozilla.org/timer;1"].createInstance(Ci.nsITimer);
-            this.previewTab = gBrowser.selectedTab;
-            this.onTabClose = this.onTabClose.bind(this);
-            tabs.addEventListener("TabClose", this.onTabClose);
+            this.onMouseDown = this._onMouseDown;
+            this.onTabCloseHide = this.onTabCloseHide.bind(this);
+            tabs.addEventListener("TabClose", this.onTabCloseHide);
+            tabs.addEventListener("TabHide", this.onTabCloseHide);
+            this.onTabSelect = this.onTabSelect.bind(this);
+            tabs.addEventListener("TabSelect", this.onTabSelect);
+            this.pwTab = gBrowser.selectedTab;
         }
         this.onMouseIn = this.onMouseIn.bind(this);
         tabs.addEventListener("mouseover", this.onMouseIn);
         this.onMouseOut = this.onMouseOut.bind(this);
         tabs.addEventListener("mouseout", this.onMouseOut);
-        this.onMouseClicked = this.onMouseClicked.bind(this);
-        tabs.addEventListener("click", this.onMouseClicked);
+        this.onMouseDown = this.onMouseDown.bind(this);
+        tabs.addEventListener("mousedown", this.onMouseDown, true);
     },
     destructor() {
         var {tabs} = this;
         tabs.removeEventListener("mouseover", this.onMouseIn);
         tabs.removeEventListener("mouseout", this.onMouseOut);
-        tabs.removeEventListener("click", this.onMouseClicked);
+        tabs.removeEventListener("mousedown", this.onMouseDown, true);
         if (!previewmode) return;
-        tabs.removeEventListener("TabClose", this.onTabClose);
+        tabs.removeEventListener("TabClose", this.onTabCloseHide);
+        tabs.removeEventListener("TabHide", this.onTabCloseHide);
+        tabs.removeEventListener("TabSelect", this.onTabSelect);
     },
-    callback(tab) {
-        gBrowser.selectedTab = tab;
-        this.target = false;
+    callback(e) {
+        var tab = e.target.closest?.("tab:not([selected])");
+        if (tab) gBrowser.selectedTab = tab;
     },
-    previewCallback(tab) {
-        gBrowser.selectedTab = tab;
+    previewCallback() {
+        gBrowser.selectedTab = this.pwTab;
     },
     onMouseIn(e) {
-        var tab = e.target.closest?.("tab.tabbrowser-tab:not([selected])");
-        if (!tab) return;
-        this.target = true;
-        this.tid.initWithCallback(() => this.callback(tab), delay, this.tid.TYPE_ONE_SHOT);
+        this.tid.initWithCallback(() => this.callback(e), delay, this.type);
     },
     _onMouseIn(e) {
-        this.previewTid.cancel();
-        var tab = e.target.closest?.("tab.tabbrowser-tab:not([selected])");
-        if (!tab) return;
-        this.target = true;
-        this.tid.initWithCallback(() => this.callback(tab), delay, this.tid.TYPE_ONE_SHOT);
+        this.pwTid.cancel();
+        this.tid.initWithCallback(() => this.callback(e), delay, this.type);
     },
     onMouseOut() {
         this.tid.cancel();
-        this.target = false;
     },
     _onMouseOut() {
         this.tid.cancel();
-        this.target = false;
-        if (!this.previewTab) return;
-        this.previewTid.cancel();
-        this.previewTid.initWithCallback(() => this.previewCallback(this.previewTab), returndelay, this.previewTid.TYPE_ONE_SHOT);
+        this.pwTid.initWithCallback(() => this.previewCallback(), returndelay, this.type);
     },
-    onMouseClicked(e) {
-        if (clickreloadtab && e.detail === 1 && e.button === 0 && !this.target && e.composedTarget.matches("tab.tabbrowser-tab :not(toolbarbutton, image):scope, tab.tabbrowser-tab image.tab-icon-image:scope"))
-            this.reload.doCommand();
+    onMouseDown(e) {
         this.tid.cancel();
-        this.target = false;
+        var tab = e.target.closest?.("tab[selected]");
+        if (!tab) return;
+        if (clickreloadtab && e.button === 0
+            && e.composedTarget.matches?.("tab :not(toolbarbutton, image:not(.tab-icon-image)):scope")) this.reload.doCommand();
     },
-    _onMouseClicked(e) {
-        this.previewTid.cancel();
-        var tab = gBrowser.selectedTab;
-        if (clickreloadtab && e.detail === 1 && e.button === 0 && !this.target && this.previewTab == tab && e.composedTarget.matches("tab.tabbrowser-tab :not(toolbarbutton, image):scope, tab.tabbrowser-tab image.tab-icon-image:scope"))
-            this.reload.doCommand();
+    _onMouseDown(e) {
         this.tid.cancel();
-        this.target = false;
-        this.previewTab = tab;
+        this.pwTid.cancel();
+        var tab = e.target.closest?.("tab[selected]");
+        if (!tab) return;
+        if (clickreloadtab && e.button === 0 && this.pwTab == tab
+            && e.composedTarget.matches?.("tab :not(toolbarbutton, image:not(.tab-icon-image)):scope")) this.reload.doCommand();
+        this.pwTab = tab;
     },
-    onTabClose(e) {
-        if (e.target == this.previewTab) this.previewTab = gBrowser.selectedTab;
+    onTabCloseHide(e) {
+        if (e.target == this.pwTab) this.pwTab = gBrowser.selectedTab;
+    },
+    onTabSelect(e) {
+        if (!this.tid.callback) this.pwTab = e.target;
     },
 }).init())();

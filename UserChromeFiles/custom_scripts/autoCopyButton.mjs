3c8d0fcca84c890b3421cd48da2555e49453019d
VitaliyVstyle <serials101@gmail.com>
2026-02-22 00:48:08 +0300

    2026.02.22

--- UserChromeFiles/custom_scripts/autoCopyButton.mjs
+++ UserChromeFiles/custom_scripts/autoCopyButton.mjs
@@ -5,7 +5,7 @@ const lazy = {
     id: "ucf-auto-copy-button",
     label: "autoCopyButton",
     tooltiptext: "Left-click: Toggle auto-copy\nMidle-click | Right-click: Toggle auto-copy on the current page",
-    image: "data:image/svg+xml;charset=utf-8,<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 16 16' fill='context-fill rgb(142, 142, 152)' fill-opacity='context-fill-opacity'><path d='M6 0a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm0 1h6a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1M3 2.268C2.402 2.614 2 3.26 2 4v8.5A3.5 3.5 0 0 0 5.5 16H10c.74 0 1.387-.402 1.732-1H5.5A2.5 2.5 0 0 1 3 12.5V2.27z'/></svg>",
+    image: "data:image/svg+xml;charset=utf-8,<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 16 16'><path style='fill:none;stroke:context-fill rgb(142, 142, 152);stroke-opacity:context-fill-opacity;stroke-width:1.2;stroke-linecap:round;stroke-linejoin:round;' d='M6 .5h6c.8 0 1.5.7 1.5 1.5v10c0 .8-.7 1.5-1.5 1.5H6c-.8 0-1.5-.7-1.5-1.5V2c0-.8.7-1.5 1.5-1.5m5.5 15H5c-1.5 0-2.5-1-2.5-2.5V2.5'/></svg>",
     pref: "ucf.auto_copy.disabled",
     copyToClipboard: true,
     copyToSearchbar: false,

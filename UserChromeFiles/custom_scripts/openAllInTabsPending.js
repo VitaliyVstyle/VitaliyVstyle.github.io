4393e33aebe1a056b33d15cd631b778f0cb32268
VitaliyVstyle <serials101@gmail.com>
2026-03-07 21:03:55 +0300

    2026.03.07

--- UserChromeFiles/custom_scripts/openAllInTabsPending.js
+++ UserChromeFiles/custom_scripts/openAllInTabsPending.js
@@ -3,8 +3,8 @@
 @UCF @param {"prop":"JsContent.pageshow","urlregxp":"^chrome:\\/\\/browser\\/content\\/(?:browser|places\\/(?:bookmarksSidebar|historySidebar|places))\\.xhtml"} @UCF
 */
 (async (
-    id = "placesContext_openAllInTabs",
-    label = "Open All in Tabs Pending",
+    id = "placesContext_openInTabsPending",
+    label = "Open in Tabs Pending",
     image = "chrome://browser/skin/tabs.svg",
     propertiesPending = `
 text-decoration-line: underline !important;
@@ -23,35 +23,38 @@ opacity: .5 !important;
         item.id = id;
         item.label = label;
         if (image) item.className = "menuitem-iconic";
-        item.onclick = this.open.bind(this, elm.parentElement);
-        Object.defineProperty(item, "hidden", {});
-        Object.defineProperty(item, "disabled", {});
+        item.setAttribute("selection-type", "any");
+        item.setAttribute("node-type", "query|folder|link|separator");
+        var desc = Object.getOwnPropertyDescriptor(XULElement.prototype, "hidden");
+        var popup = elm.parentElement;
+        desc.set = this.hidden.bind(item, popup, desc.set);
+        Object.defineProperty(item, "hidden", desc);
+        item.onclick = this.open.bind(this, popup);
         var style = `data:text/css;charset=utf-8,${encodeURIComponent(`
-#placesContext {
-#${id} {${image ? `
+#placesContext > #${id} {${image ? `
 --menuitem-icon: url("${image}") !important;
 list-style-image: url("${image}") !important;
 -moz-context-properties: fill !important;
 fill: currentColor !important;` : ""}
-display: none;
-}
-:is([id="placesContext_openBookmarkContainer:tabs"],[id="placesContext_openBookmarkLinks:tabs"],
-[id="placesContext_openContainer:tabs"],[id="placesContext_openLinks:tabs"]):not([hidden],[disabled]) ~ #${id} {
-display: revert;
-}
 }
 ${propertiesPending ? `:root[windowtype="navigator:browser"] .tabbrowser-tab:not([selected],[multiselected])[pending] .tab-label {${propertiesPending}}` : ""}
 `)}`;
         windowUtils.loadSheetUsingURIString(style, windowUtils.USER_SHEET);
         elm.after(item);
     },
+    hidden({ triggerNode: tn, _view: vw }, set, val) {
+        var node = tn?._placesView?.result.root || vw.selectedNode || vw.result.root;
+        if (PlacesUtils.nodeIsContainer(node) && !PlacesUtils.hasChildURIs(node)) val = true;
+        set.call(this, val);
+    },
     async open({ triggerNode: tn, _view: vw }, e) {
-        var nodes;
-        if (tn.matches("treechildren")) nodes = vw.selectedNode || vw.selectedNodes;
-        else if (tn._placesNode && PlacesUtils.nodeIsContainer(tn._placesNode)) nodes = tn._placesNode;
-        if (!nodes && !nodes?.length) nodes = vw.result.root;
-        var items = PlacesUtils.nodeIsContainer(nodes)
-            ? PlacesUtils.getURLsForContainerNode(nodes) : Array.from(nodes).filter(PlacesUtils.nodeIsURI);
+        var nodes = tn._placesNode || tn._placesView?.result.root || vw.selectedNodes;
+        if (nodes.length === 0) nodes = vw.result.root;
+        var items = [];
+        for (let node of Array.isArray(nodes) ? nodes : [nodes]) {
+            if (PlacesUtils.nodeIsContainer(node)) items.push(...PlacesUtils.getURLsForContainerNode(node));
+            else if (PlacesUtils.nodeIsURI(node)) items.push(node);
+        }
         if (!items.length) return;
         var win = vw.ownerWindow;
         win = win?.document.documentElement.getAttribute("windowtype") === "navigator:browser"
